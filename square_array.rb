c02e5ec83a292adbd8431294c9af683498944f94
Seth Todd <seth.todd@outlook.com>
2020-01-21 03:50:13 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ array = [2,4,6,3]
 def square_array(array)
   new_array = []
   array.each do |squares|
-   new_array = squares**2
-   puts new_array
-   end
-end
+   new_array << squares**2
+ end
+ end
+ 
